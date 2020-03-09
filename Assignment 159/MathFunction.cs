559d7611d1ce8f4b669cfb6eacec348dc6bd5d0a
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-08 18:52:21 -0700

    Add project files.

---
+++ Assignment 159/MathFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_159
+{
+    public class MathFunction
+    {
+        public int mathOne(int num1)
+        {
+            int num2 = 5;
+            var Total = num1 * num2;
+            return Total;
+        }
+
+        public int mathOne(decimal num1)
+        {
+            decimal num2 = 2.00m;
+            var answer = Convert.ToInt32(num1 / num2);
+            return answer;
+        }
+
+        public int mathOne(string stringOne)
+        {
+            int num3 = 12;
+            int num4 = Convert.ToInt32(stringOne);
+            int conclusion = num4 - num3;
+            return conclusion;
+        }
+    }
+}
---
+++ Assignment 159/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 5;
+            int Total;
+            MathFunction functionOne = new MathFunction();
+
+            Total = functionOne.mathOne(z);
+
+            Console.WriteLine("This is Step 1-2. multiply 5 * 5.");
+            Console.WriteLine(Total);
+
+            decimal q = 43.57m;
+            int answer;
+            MathFunction functionTwo = new MathFunction();
+            answer = functionTwo.mathOne(q);
+
+            Console.WriteLine("This is Step 3-4. divide 43.57 by 2.00");
+            Console.WriteLine(answer);
+
+
+            string x;
+            int conclusion;
+            MathFunction functionThree = new MathFunction();
+            Console.WriteLine("Enter a number for the final Method:");
+            x = (Console.ReadLine());
+            Console.WriteLine(x + " minus 12 equals:");
+            conclusion = functionThree.mathOne(x);
+
+            Console.WriteLine(conclusion);
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
